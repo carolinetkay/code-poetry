dd723b9ddc8e52f6786dbf15104fa161b400faba
Caroline Tan <caroline.kh.tan@gmail.com>
2021-03-01 23:55:48 +0800

    made url query dynamic

--- code-poetry-3/src/index.js
+++ code-poetry-3/src/index.js
@@ -2,14 +2,13 @@ const button = document.querySelector(".button");
 const text = document.querySelector("p");
 
 button.addEventListener('click', (event) => {
-    let params = encodeURIComponent("Is today going to be a good day?");
-    let url = "https://8ball.delegator.com/magic/JSON/" + params;
+    const question = encodeURIComponent(document.querySelector("input").value);
+    const url = "https://8ball.delegator.com/magic/JSON/" + question;
     fetch(url)
         .then(response => response.json())
         .then((json) => {
             text.style.display = "none";
             const results = `<p style="margin-top: 40vh; text-align: center; font: normal 24px monospace;">${json['magic']['answer']}</p>`;
-            // console.log(json['magic']['answer'])
             text.insertAdjacentHTML('afterend', results);
         });
 });
