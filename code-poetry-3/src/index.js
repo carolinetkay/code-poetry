5ccea9525506601a65b96bdd81bb409041389f49
Caroline Tan <caroline.kh.tan@gmail.com>
2021-03-01 23:46:59 +0800

    magic 8 ball api

--- code-poetry-3/src/index.js
+++ code-poetry-3/src/index.js
@@ -2,6 +2,14 @@ const button = document.querySelector(".button");
 const text = document.querySelector("p");
 
 button.addEventListener('click', (event) => {
-    text.style.display = "none";
-    text.insertAdjacentHTML('afterend', '<p style="margin-top: 40vh; text-align: center; font: normal 24px monospace;">Your imagination</p>');
+    let params = encodeURIComponent("Is today going to be a good day?");
+    let url = "https://8ball.delegator.com/magic/JSON/" + params;
+    fetch(url)
+        .then(response => response.json())
+        .then((json) => {
+            text.style.display = "none";
+            const results = `<p style="margin-top: 40vh; text-align: center; font: normal 24px monospace;">${json['magic']['answer']}</p>`;
+            // console.log(json['magic']['answer'])
+            text.insertAdjacentHTML('afterend', results);
+        });
 });
